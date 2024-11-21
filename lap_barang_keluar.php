c9b55af61956df2bca47bcadcf51663e925f6e74
Ismatullahza <145858405+Ismatullahza@users.noreply.github.com>
2024-11-21 10:28:47 +0700

    Add files via upload

--- lap_barang_keluar.php
+++ lap_barang_keluar.php
@@ -1,87 +1,38 @@
-<?php
-// session_start();
-include ('../config/conn.php');
-include ('../config/function.php');
-?>
-<html>
+<?php hakAkses(['admin']); ?>
+<!-- Begin Page Content -->
+<div class="container-fluid">
 
-<head>
-    <style>
-    h2 {
-        padding: 0px;
-        margin: 0px;
-        font-size: 14pt;
-    }
-
-    h4 {
-        font-size: 12pt;
-    }
-
-    text {
-        padding: 0px;
-    }
-
-    table {
-        border-collapse: collapse;
-        border: 1px solid #000;
-        font-size: 11pt;
-    }
-
-    th,
-    td {
-        border: 1px solid #000;
-        padding: 5px;
-    }
-
-    table.tab {
-        table-layout: auto;
-        width: 100%;
-    }
-    </style>
-    <title>Cetak Laporan Barang Keluar</title>
-</head>
-
-<body>
-    <?php
-    $tgl_awal = $_POST['tanggal_awal'];
-    $tgl_akhir = $_POST['tanggal_akhir'];
-    $query = mysqli_query($con,"SELECT x.*,x1.nama_barang,x2.nama_merek,x3.nama_kategori FROM barang_keluar x JOIN barang x1 ON x1.idbarang=x.barang_id JOIN merek x2 ON x2.idmerek=x1.merek_id JOIN kategori x3 ON x3.idkategori=x1.kategori_id WHERE x.tanggal BETWEEN '$tgl_awal' AND '$tgl_akhir' ORDER BY x.idbarang_keluar DESC")or die(mysqli_error($con));
-    
-    ?>
-    <div style="page-break-after:always;text-align:center;margin-top:5%;">
-        <div style="line-height:5px;">
-            <h2>LAPORAN BARANG KELUAR INVENTARIS PT. JAVA MEDIANET TEKHNOLOGY</h2>
-            <h4>TANGGAL "<?= date('d-m-Y',strtotime($tgl_awal)); ?>" SAMPAI
-                "<?= date('d-m-Y',strtotime($tgl_akhir)); ?>"</h4>
-        </div>
-        <hr style="border-color:black;">
-        <table class="tab">
-            <tr>
-                <th width="20">NO</th>
-                <th>TGL</th>
-                <th>NAMA BARANG</th>
-                <th>MEREK</th>
-                <th>KATEGORI</th>
-                <th>KETERANGAN</th>
-                <th>JUMLAH</th>
-            </tr>
-            <?php $n=1; while($row = mysqli_fetch_array($query)): ?>
-            <tr>
-                <td> align="center"><?= $n++.'.'; ?></td>
-                <td><?= date('d-m-Y',strtotime($row['tanggal'])); ?></td>
-                <td><?= $row['nama_barang']; ?></td>
-                <td><?= $row['nama_merek']; ?></td>
-                <td><?= $row['nama_kategori']; ?></td>
-                <td><?= $row['keterangan']; ?></td>
-                <td><?= $row['jumlah']; ?></td>
-            </tr>
-            <?php endwhile; ?>
-        </table>
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Laporan Barang Keluar</h1>
     </div>
-</body>
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+        <div class="card-body">
+            <form action="<?=base_url();?>process/lap_barang_keluar.php" method="post" target="_blank">
+                <div class="row">
+                    <div class="col-md-5">
+                        <div class="form-group">
+                            <label for="tanggal_awal">Tanggal Awal</label>
+                            <input type="date" name="tanggal_awal" id="tanggal_awal" class="form-control"
+                                value="<?=date('Y-m-d');?>">
+                        </div>
+                    </div>
+                    <div class="col-md-5">
+                        <div class="form-group">
+                            <label for="tanggal_akhir">Tanggal Akhir</label>
+                            <input type="date" name="tanggal_akhir" id="tanggal_akhir" class="form-control"
+                                value="<?=date('Y-m-d');?>">
+                        </div>
+                    </div>
+                    <div class="col-md-2 p-2">
+                        <button type="submit" class="btn btn-info mt-4"><i class="fas fa-print"></i> Cetak
+                            Laporan</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 
-</html>
-
-<script>
-window.print();
-</script>
+</div>
+<!-- /.container-fluid -->
--- pengguna.php
+++ pengguna.php
@@ -1,16 +1,22 @@
-<?php hakAkses(['admin']); ?>
+<?php hakAkses(['admin']) ?>
 <script>
 function submit(x) {
     if (x == 'add') {
-        $('[name="nama_barang"]').val("");
-        $('[name="merek_id"]').val("").trigger('change');
-        $('[name="kategori_id"]').val("").trigger('change');
-        $('[name="keterangan"]').val("");
-        $('#barangModal .modal-title').html('Tambah Barang');
+        $('[name="username"]').val("");
+        $('[name="nama"]').val("");
+        $('[name="no_hp"]').val("");
+        // $('[name="level"]').val("");
+        $('#penggunaModal .modal-title').html('Tambah Pengguna');
+        $('[name="username"]').prop('readonly', false);
+        $('[name="password"]').prop('required', true);
+        $('#passwordHelp').hide();
         $('[name="ubah"]').hide();
         $('[name="tambah"]').show();
     } else {
-        $('#barangModal .modal-title').html('Edit Barang');
+        $('#penggunaModal .modal-title').html('Edit Pengguna');
+        $('[name="username"]').prop('readonly', true);
+        $('[name="password"]').prop('required', false);
+        $('#passwordHelp').show();
         $('[name="tambah"]').hide();
         $('[name="ubah"]').show();
 
@@ -19,14 +25,14 @@ function submit(x) {
             data: {
                 id: x
             },
-            url: '<?=base_url();?>process/view_barang.php',
+            url: '<?=base_url();?>process/view_user.php',
             dataType: 'json',
             success: function(data) {
-                $('[name="idbarang"]').val(data.idbarang);
-                $('[name="merek_id"]').val(data.merek_id).trigger('change');
-                $('[name="kategori_id"]').val(data.kategori_id).trigger('change');
-                $('[name="nama_barang"]').val(data.nama_barang);
-                $('[name="keterangan"]').val(data.keterangan);
+                $('[name="id"]').val(data.id_users);
+                $('[name="username"]').val(data.username);
+                $('[name="nama"]').val(data.nama);
+                $('[name="no_hp"]').val(data.no_hp);
+                // $('[name="level"]').val(data.level);
             }
         });
     }
@@ -37,12 +43,12 @@ function submit(x) {
 
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Barang</h1>
+        <h1 class="h3 mb-0 text-gray-800">Pengguna</h1>
     </div>
     <!-- DataTales Example -->
     <div class="card shadow mb-4">
         <div class="card-header py-3">
-            <a href="#" class="btn btn-primary btn-icon-split btn-sm" data-toggle="modal" data-target="#barangModal"
+            <a href="#" class="btn btn-primary btn-icon-split btn-sm" data-toggle="modal" data-target="#penggunaModal"
                 onclick="submit('add')">
                 <span class="icon text-white-50">
                     <i class="fas fa-plus"></i>
@@ -56,31 +62,28 @@ function submit(x) {
                     <thead>
                         <tr>
                             <th width="20">NO</th>
-                            <th>NAMA BARANG</th>
-                            <th>MEREK</th>
-                            <th>KATEGORI</th>
-                            <th>KETERANGAN</th>
-                            <th>STOK</th>
+                            <th>NAMA LENGKAP</th>
+                            <th>TELP</th>
+                            <th>USERNAME</th>
+                            <!-- <th>LEVEL</th> -->
                             <th width="50">AKSI</th>
                         </tr>
                     </thead>
                     <tbody>
                         <?php 
                         $n=1;
-                        $query = mysqli_query($con,"SELECT x.*,x1.nama_merek,x2.nama_kategori FROM barang x JOIN merek x1 ON x1.idmerek=x.merek_id JOIN kategori x2 ON x2.idkategori=x.kategori_id ORDER BY x.idbarang DESC")or die(mysqli_error($con));
+                        $query = mysqli_query($con,"SELECT * FROM users ORDER BY id_users DESC")or die(mysqli_error($con));
                         while($row = mysqli_fetch_array($query)):
                         ?>
                         <tr>
                             <td><?= $n++; ?></td>
-                            <td><?= $row['nama_barang']; ?></td>
-                            <td><?= $row['nama_merek']; ?></td>
-                            <td><?= $row['nama_kategori']; ?></td>
-                            <td><?= $row['keterangan']; ?></td>
-                            <td><?= $row['stok']; ?></td>
+                            <td><?= $row['nama']; ?></td>
+                            <td><?= $row['no_hp']; ?></td>
+                            <td><?= $row['username']; ?></td>
                             <td>
-                                <a href="#barangModal" data-toggle="modal" onclick="submit(<?=$row['idbarang'];?>)"
+                                <a href="#penggunaModal" data-toggle="modal" onclick="submit(<?=$row['id_users'];?>)"
                                     class="btn btn-sm btn-circle btn-info"><i class="fas fa-edit"></i></a>
-                                <a href="<?=base_url();?>/process/barang.php?act=<?=encrypt('delete');?>&id=<?=encrypt($row['idbarang']);?>"
+                                <a href="<?=base_url();?>/process/users.php?act=<?=encrypt('delete');?>&id=<?=encrypt($row['id_users']);?>"
                                     class="btn btn-sm btn-circle btn-danger btn-hapus"><i class="fas fa-trash"></i></a>
                             </td>
                         </tr>
@@ -94,12 +97,12 @@ function submit(x) {
 </div>
 <!-- /.container-fluid -->
 
-<!-- Modal Tambah barang -->
-<div class="modal fade" id="barangModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+<!-- Modal Tambah Pengguna -->
+<div class="modal fade" id="penggunaModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
     aria-hidden="true" data-backdrop="static">
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
-            <form action="<?=base_url();?>process/barang.php" method="post">
+            <form action="<?=base_url();?>process/users.php" method="post">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel"></h5>
                     <button class="close" type="button" data-dismiss="modal" aria-label="Close">
@@ -110,38 +113,41 @@ function submit(x) {
                     <div class="row">
                         <div class="col-md-12">
                             <div class="form-group">
-                                <label for="nama_barang">Nama Barang <span class="text-danger">*</span></label>
-                                <input type="hidden" name="idbarang" class="form-control">
-                                <input type="text" class="form-control" id="nama_barang" name="nama_barang" required>
+                                <label>Nama Lengkap</label>
+                                <input type="hidden" name="id" class="form-control">
+                                <input type="text" class="form-control" name="nama" required>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="merek_id">Merek Barang <span class="text-danger">*</span></label>
-                                <select name="merek_id" id="merek_id" class="form-control select2" style="width:100%;"
-                                    required>
-                                    <option value="">-- Pilih Merek --</option>
-                                    <?= list_merek(); ?>
-                                </select>
+                                <label>Nomor Telepon</label>
+                                <input type="text" class="form-control" name="no_hp" required>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="kategori_id">Kategori Barang <span class="text-danger">*</span></label>
-                                <select name="kategori_id" id="kategori_id" class="form-control select2"
-                                    style="width:100%;" required>
-                                    <option value="">-- Pilih Kategori --</option>
-                                    <?= list_kategori(); ?>
-                                </select>
+                                <label>Username</label>
+                                <input type="text" class="form-control" name="username" required>
                             </div>
                         </div>
                         <div class="col-md-12">
                             <div class="form-group">
-                                <label for="keterangan">Keterangan <span class="text-danger">*</span></label>
-                                <textarea name="keterangan" id="keterangan" cols="30" rows="5" class="form-control"
-                                    required></textarea>
+                                <label>Password</label>
+                                <input type="password" class="form-control" name="password"
+                                    aria-describedby="passwordHelp">
+                                <small id="passwordHelp" class="form-text" style="color:red;">Biarkan kosong
+                                    jika tidak ingin merubah password</small>
                             </div>
                         </div>
+                        <!-- <div class="col-md-6">
+                            <div class="form-group">
+                                <label>Level Akses</label>
+                                <select name="level" class="form-control" required>
+                                    <option value="user">Staff Pegawai</option>
+                                    <option value="admin">Administrator</option>
+                                </select>
+                            </div>
+                        </div> -->
                     </div>
                     <hr class="sidebar-divider">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal"><i class="fas fa-times"></i>
